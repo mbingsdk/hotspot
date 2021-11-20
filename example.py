b180e7e148c301f9713096926a65f71ae9440524
Tamin Robelcom <53684389+mbingsdk@users.noreply.github.com>
2021-11-20 16:37:56 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,1 +1,7 @@
-from hotspot.autoConfiguration import
+from hotspot.autoConfiguration import *
+
+# Connection
+a = autoHotspot("admin", "admin", "192.168.1.1", "8728", "192.168.1.3/24", "192.168.10.1/24", "192.168.11.1/24", "8.8.8.8,8.8.4.4", "vx6-ct.net")
+
+# Simple Mikrotik Hotspot Automatic Configuration
+a.run()
